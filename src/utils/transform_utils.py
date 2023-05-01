63b518e43602e10bf5eb9fef88cf2aaae3c0e5b5
abuh1 <abuhamza2308@hotmail.com>
2023-05-01 16:30:34 +0100

    Tested clean data function in utils

--- src/utils/transform_utils.py
+++ src/utils/transform_utils.py
@@ -44,15 +44,52 @@ def load_data(input_file, table_name=''):
     df = pd.DataFrame(data)
     return df
 
-# takes list of columns from df to convert to a dtype
-def convert_column_type(df, cols, type):
-    pass
-
-# drops specified columns from the df
-def drop_columns(df, cols):
-    pass
-
-# drops specified rows from the df
-def drop_rows(df, cols):
-    pass
-
+def clean_data(df,
+               drop_null_columns=False,
+               drop_null_rows=False,
+               cols_to_drop=[],
+               rows_to_drop=[],
+               remove_duplicates=False,
+               convert_cols={}):
+    """
+    Performs cleaning operations on a pandas DataFrame.
+    
+    Args:
+        df (pandas.DataFrame): The DataFrame to clean.
+        drop_null_columns (bool): Whether to drop columns that contain null values or not.
+        drop_null_rows (bool): Whether to drop rows that contain null values or not.
+        cols_to_drop (list): Optional columns to drop.
+        rows_to_drop (list): Optional rows to drop.
+        remove_duplicates (bool): Removes any duplicate entries if set to True.
+        convert_cols (dict): Column (key) and dtype to convert the column into (value) to convert columns' data types.
+        
+    Returns:
+        df (pandas.DataFrame): Cleaned data.
+    """
+    # type check df
+    if not isinstance(df, (pd.DataFrame, pd.Series)):
+        raise TypeError("The 'df' parameter must be a pandas DataFrame or Series.")
+    
+    df_copy = df.copy()
+    
+    # drop null columns and rows
+    if drop_null_columns:
+        df_copy.dropna(axis=1, inplace=True)
+    if drop_null_rows:
+        df_copy.dropna(axis=0, inplace=True)
+    
+    # drops columns and rows specified
+    df_copy.drop(cols_to_drop, axis=1, inplace=True)
+    df_copy.drop(rows_to_drop, axis=0, inplace=True)
+    
+    # remove duplicates
+    if remove_duplicates:
+        df_copy.drop_duplicates(inplace=True)
+    
+    # convert specified columns to target dtype
+    for col, dtype in convert_cols.items():
+        if dtype == "int" or dtype == "float":
+            df_copy[col] = pd.to_numeric(df_copy[col], errors="coerce")
+        df_copy[col] = df_copy[col].astype(dtype)
+    
+    return df_copy
--- tests/test_transform_utils.py
+++ tests/test_transform_utils.py
@@ -2,12 +2,17 @@ import unittest
 import pandas as pd
 import sys
 sys.path.insert(0, '../')
-from src.utils.transform_utils import load_data
+from src.utils.transform_utils import load_data, clean_data
 
 class TestTransformUtils(unittest.TestCase):
 
     def setUp(self):
-        pass
+        # sample dataframe for testing
+        data = {'Name': ['John', 'Jane', 'Jim', 'Jake', 'Jill', 'Jill'],
+                'Age': [25, 30, 35, 40, None, None],
+                'Gender': ['M', 'F', 'M', 'M', 'F', 'F'],
+                'Salary': [50000.0, 60000.0, None, 70000.0, 80000.0, 80000.0]}
+        self.df = pd.DataFrame(data)
     
     def test_csv_loading(self):
         # test loading csv file
@@ -27,6 +32,25 @@ class TestTransformUtils(unittest.TestCase):
         df = load_data(input_file, table_name='person')
         self.assertIsInstance(df, pd.DataFrame)
     
+    def test_clean_data(self):
+        # test that the function removes null columns and rows
+        cleaned_cols_df = clean_data(self.df, drop_null_columns=True)
+        self.assertEqual(cleaned_cols_df.shape, (6, 2))
+        cleaned_rows_df = clean_data(self.df, drop_null_rows=True)
+        self.assertEqual(cleaned_rows_df.shape, (3,4))
+        
+        # test dropping specified columns and rows
+        dropped_rows_cols_df = clean_data(self.df, cols_to_drop=['Salary'], rows_to_drop=[3])
+        self.assertEqual(dropped_rows_cols_df.shape, (5, 3))
+        
+        # test remove duplicates
+        remove_dupes_df = clean_data(self.df, remove_duplicates=True)
+        self.assertEqual(remove_dupes_df.shape, (5, 4))
+        
+        # test converting columns
+        converted_df = clean_data(self.df, convert_cols={'Age': 'float'})
+        self.assertEqual(converted_df.dtypes['Age'], 'float64')
+        
     def tearDown(self):
         pass
     
